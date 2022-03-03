e737be7a6f318b5a7a00a7966c30ed34d95a5677
KamaliHari <100555747+KamaliHari@users.noreply.github.com>
2022-03-03 13:30:07 +0530

    Update Ebbill.sql

--- Ebbill.sql
+++ Ebbill.sql
@@ -1,18 +1,21 @@
-CREATE DATABASE Eb_Bill;
-USE Eb_Bill;
+CREATE DATABASE EbBill;
+USE EbBill;
+
 
 CREATE TABLE Customer_info(
 
-id int auto_increment primary key,
+id int(50) not null auto_increment,
 firstname varchar(30) NOT NULL,
 lastname varchar(30)NOT NULL,
 address varchar(30)NOT NULL,
-UserID INT DEFAULT 9,
+UserID INT (10000) NOT NULL primary key,
 password varchar(30)NOT NULL,
 type ENUM('H','C') NOT NULL
 );
+alter table customer_info ADD UNIQUE INDEX(UserId);
 
 SHOW TABLES ;
 
 SELECT * FROM Customer_info;
-INSERT INTO Customer_info (firstname,lastname,address,password,type) values ('xxxxx','H','abc','@13124df','H');
+INSERT IGNORE INTO Customer_info (firstname,lastname,address,UserID,password,type) values ('adjfh','H','abc','1','@13124df','H');
+DELETE FROM Customer_info WHERE firstName='xxxxx';
