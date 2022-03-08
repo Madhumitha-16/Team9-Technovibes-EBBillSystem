eb181c31e3774d1a5ae7f6e4852a6b36585f70b5
dbcodesxo <100557450+dbcodesxo@users.noreply.github.com>
2022-03-08 20:45:48 +0530

    Update Ebbill.sql

--- Ebbill.sql
+++ Ebbill.sql
@@ -11,11 +11,13 @@ firstname varchar(30) NOT NULL,
 lastname varchar(30)NOT NULL,
 address varchar(30)NOT NULL,
 UserID INT (255) NOT NULL,
-password varchar(30)NOT NULL,
 type ENUM('H','C') NOT NULL,
+previousMonthReading int(255),
+CurrentMonthReading int(255),
+status varchar(30) not null,
 Primary key(id,UserID)
 );
+
 ALTER TABLE Customer
 MODIFY COLUMN status varchar(30) default 'Unpaid';
-delete from Customer where id='8';
 alter table Customer add totalAmount int(255) after CurrentMonthReading;
