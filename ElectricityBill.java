c90a218028cd0328e733fe70c84d828b28b4fc0b
Madhumitha-16 <75004278+Madhumitha-16@users.noreply.github.com>
2022-03-08 12:21:30 +0530

    Updated Calculation

--- ElectricityBill.java
+++ ElectricityBill.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.Scanner;
 
-public class ElectricityBill {
+public class SQL {
 	Scanner sc = new Scanner(System.in) ;
 	public static void main(String[] args)throws Exception {
         try{
@@ -33,8 +33,8 @@ public class ElectricityBill {
             	   break;
                case 4:
             	   System.out.println("Enter Consumer Number: ");
-//            	   int consumer_No=sc.nextInt();
-//            	   calculateBillAmount(url,username,pass,consumer_No);
+            	   int consumer_No=sc.nextInt();
+            	   calculateBillAmount(url,username,pass,consumer_No);
             	   System.out.println("Exiting......\nExited");
                    System.exit(0);
                default:
@@ -59,14 +59,14 @@ public class ElectricityBill {
 		    System.out.println("Enter your Address : ");
 		    String address = sc.next();
 		    System.out.println("Enter your Mobile Number : ");
-		    int mobile_No = sc.nextInt();
+		    int mobileNo = sc.nextInt();
             System.out.println("Enter your User ID : ");
 		    int UserID = sc.nextInt();
 		    System.out.println("Enter your previous Month Reading : ");
 		    int previousMonthReading = sc.nextInt();
 		    System.out.println("Enter your Current Month Reading : ");
 		    int CurrentMonthReading = sc.nextInt();
-		    System.out.println("Enter your Type of Connection :");
+		    System.out.println("Enter your Type of Connection \n(H - Household)\n(C - Commercial  :");
 		    String type = sc.next();
 		    Statement statement = connection.createStatement();
 		    String queryCheck = "SELECT * from Customer WHERE UserID = '" +UserID+ "' " ;
@@ -94,76 +94,83 @@ public class ElectricityBill {
 		Statement statement = connection.createStatement();
 		ResultSet resultSet = statement.executeQuery("select * from Customer where UserID='"+consumer_no+"' ");
 		boolean res=resultSet.next();
+		
+		
 		if(res) {
-			int Cur_Unit=resultSet.getInt(9);
-			int Prev_Unit=resultSet.getInt(8);
+			int Cur_Unit=resultSet.getInt(10);
+			int Prev_Unit=resultSet.getInt(9);
 			int units=Cur_Unit - Prev_Unit;
-			System.out.println("---------------------------BILL RECEIPT------------------------------");
-			System.out.println("Consumer No."+"   "+"First Name"+"   "+"Last Name"+"       "+"Units Consumed"+"   "+"Status");
-			System.out.println("---------------------------------------------------------------------");
-            System.out.println(resultSet.getInt(5)+"                "+resultSet.getString(2)+"          "+resultSet.getString(3)+"       "+units+"       "+resultSet.getString(10));
+			double amount = calculateBillAmount(url,username,pass,consumer_no);
+			System.out.println("-----------------------------------------BILL RECEIPT----------------------------------------------");
+			System.out.println("   Consumer No."+"     "+"First Name"+"     "+"Last Name"+"       "+"Units Consumed"+"       "+"Total Amount"+"        "+"Status");
+			System.out.println("---------------------------------------------------------------------------------------------------");
+            System.out.println("     "+resultSet.getInt(6)+"                "+resultSet.getString(2)+"          "+resultSet.getString(3)+"               "+units+"                   "+amount+"           "+resultSet.getString(11));
+			System.out.println("---------------------------------------------------------------------------------------------------");
 		}else{
 			System.out.println("No such user exits");
 		}
 	}
 	
-//	private static void calculateBillAmount(String url,String username,String pass,int consumer_No) throws SQLException {
-//		Scanner sc= new Scanner(System.in);
-//		Connection connection = DriverManager.getConnection(url,username,pass);
-//		Statement statement = connection.createStatement();
-//		ResultSet resultSet = statement.executeQuery("select * from Customer where UserID='"+consumer_No+"' ");
-//		resultSet.next();
-//		String res=resultSet.getString(8);
-//		String typeCon="H";
-//		double tarrif=0.00;
-//		if(res.equals(typeCon)) {
-//					
-//			int Cur_Unit=resultSet.getInt(9);
-//			int Prev_Unit=resultSet.getInt(8);
-//			int units=Cur_Unit - Prev_Unit;
-//			if(units<101)
-//            {
-//                tarrif=units * 1.0;
-//            }
-//            else if(units>100 && units<201)
-//            {
-//                tarrif=100*1.0 + (units-100)*2.5;
-//            }
-//            else if(units>200 && units <501)
-//            {
-//                tarrif=100*1.0 + 100*2.5 + (units-200)*4.0;
-//            }
-//            else if(units>500)
-//            {
-//                tarrif=100*1.0 + 100*2.5 + 300*4.0 + (units-500)*6.0;
-//            }
-//		}else {
-//			
-//
-//			int Cur_Unit=resultSet.getInt(9);
-//			int Prev_Unit=resultSet.getInt(8);
-//			int units=Cur_Unit - Prev_Unit;
-//			if(units<101)
-//            {
-//                tarrif=units*2.0;
-//            }
-//            else if(units>100 && units<201)
-//            {
-//                tarrif=100*2.0 + (units-100)*4.50;
-//            }
-//            else if(units>200 && units<501)
-//            {
-//                tarrif=100*2.0 + 100*4.50+(units-200)*6.0;
-//            }
-//            else if(units>500)
-//            {
-//                tarrif=100*2.0 + 100*4.50 + 300*6.0 +(units-500)*7.0;
-//            }
-//        }
-//		
-//          System.out.println(String.format("%,.2f", tarrif));
-//		}
-//	
+	private static double calculateBillAmount(String url,String username,String pass,int consumer_No) throws SQLException {
+		Scanner sc= new Scanner(System.in);
+		Connection connection = DriverManager.getConnection(url,username,pass);
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from Customer where UserID='"+consumer_No+"' ");
+		resultSet.next();
+		String res=resultSet.getString(8);
+		double tarrif=0.00;
+		if(res.equals("H"))
+        {
+			int Cur_Unit=resultSet.getInt(10);
+			int Prev_Unit=resultSet.getInt(9);
+			int units=Cur_Unit - Prev_Unit;
+			System.out.println(units+"Units COnsumed Inside H");
+            if(units<101)
+            {
+                tarrif=units * 1.0;
+            }
+            else if(units>100 && units<201)
+            {
+                tarrif=100*1.0 + (units-100)*2.5;
+            }
+            else if(units>200 && units <501)
+            {
+                tarrif=100*1.0 + 100*2.5 + (units-200)*4.0;
+            }
+            else if(units>500)
+            {
+                tarrif=100*1.0 + 100*2.5 + 300*4.0 + (units-500)*6.0;
+            }
+        }
+        else
+        {
+        	int Cur_Unit=resultSet.getInt(10);
+			int Prev_Unit=resultSet.getInt(9);
+			int units=Cur_Unit - Prev_Unit;
+			System.out.println(units+ "Units Consumed");
+            if(units<101)
+            {
+                tarrif=units*2.0;
+            }
+            else if(units>100 && units<201)
+            {
+                tarrif=100*2.0 + (units-100)*4.50;
+            }
+            else if(units>200 && units<501)
+            {
+                tarrif=100*2.0 + 100*4.50+(units-200)*6.0;
+            }
+            else if(units>500)
+            {
+                tarrif=100*2.0 + 100*4.50 + 300*6.0 +(units-500)*7.0;
+            }
+        }
+		return tarrif;
+        
+    }
+        
+	
+	
 
 	
 	public static void printReceipt (String url,String username,String pass) throws ClassNotFoundException, SQLException{
