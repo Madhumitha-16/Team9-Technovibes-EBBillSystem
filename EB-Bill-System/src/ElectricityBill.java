e6c6b505d49a98edcb3bbf156ed48aa87d15d84d
Madhumitha-16 <75004278+Madhumitha-16@users.noreply.github.com>
2022-03-11 23:06:23 +0530

    Update ElectricityBill.java

--- EB-Bill-System/src/ElectricityBill.java
+++ EB-Bill-System/src/ElectricityBill.java
@@ -7,28 +7,28 @@ public class ElectricityBill {
 	Scanner sc = new Scanner(System.in) ;
 	public static void main(String[] args)throws Exception {
         try{
-        	Scanner sc = new Scanner(System.in);
-			String url = "jdbc:mysql://localhost:3306/EbBill";
-            String username = "root";
-            String pass = "madhu";
-            System.out.println("\n---------------EB-Bill-System------------------\n");
-            while(true) {
-            System.out.println("\n1. Registeration");
-            System.out.println("2. Generate Bill \n3. Payment \n4. Exit");
-            System.out.println("Enter your choice : ");
-            int c = sc.nextInt();
+		    Scanner sc = new Scanner(System.in);
+		    String url = "jdbc:mysql://localhost:3306/EbBill";
+		    String username = "root";
+		    String pass = "madhu";
+		    System.out.println("\n---------------EB-Bill-System------------------\n");
+		    while(true) {
+		    System.out.println("\n1. Registeration");
+		    System.out.println("2. Generate Bill \n3. Payment \n4. Exit");
+		    System.out.println("Enter your choice : ");
+		    int c = sc.nextInt();
                 switch(c){
                 case 1:
                 	Register reg = new Register();
-                    System.out.println("Enter Your Details");
-                    reg.register(url,username,pass);
-                    break;
+			System.out.println("Enter Your Details");
+			reg.register(url,username,pass);
+			break;
                case 2:
-            	   System.out.println("Enter Consumer Number: ");
-            	   GenerateBill genBill = new GenerateBill();
-            	   	int consumer_no=sc.nextInt();
-            	   	genBill.displayBill(url,username,pass,consumer_no);
-            	   	break;
+			System.out.println("Enter Consumer Number: ");
+			GenerateBill genBill = new GenerateBill();
+		        int consumer_no=sc.nextInt();
+			genBill.displayBill(url,username,pass,consumer_no);
+		        break;
                case 3:
             	   System.out.println("Enter Consumer Number: ");
             	   int consumer_no1=sc.nextInt();
